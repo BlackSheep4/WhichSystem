05786c6893d323a01df122dd35d813bc97f0e02e
Álvaro <76668073+XEON512@users.noreply.github.com>
2021-04-03 17:23:34 +0200

    Update WhichSystem.py
    BUGS SOLVED:
    Linux OS didn´t work. Now is working in Windows AND Linux OS
    
    THINGS ADDED:
    Try and Except added for catch the errors.

--- whichSystem.py
+++ whichSystem.py
@@ -1,21 +1,59 @@
 import subprocess
 import re
+import os
+import time
+import platform
 
+print("""
+_       ____  ______________  __   _______  _____________________  ___
+| |     / / / / /  _/ ____/ / / /  / ___/\ \/ / ___/_  __/ ____/  |/  /
+| | /| / / /_/ // // /   / /_/ /   \__ \  \  /\__ \ / / / __/ / /|_/ / 
+| |/ |/ / __  // // /___/ __  /   ___/ /  / /___/ // / / /___/ /  / /  
+|__/|__/_/ /_/___/\____/_/ /_/   /____/  /_//____//_/ /_____/_/  /_/   
+                                                                       """)
+
+time.sleep(1)
+print('\n')
 print('[+] Welcome to WhichSystem')
 target = input('[+] Please put the IP target: ')
 
+system = platform.system()
 
-ping = subprocess.Popen(f"ping {target}",shell=True,  stdout=subprocess.PIPE)
-out = ping.communicate()[0] #access stdoutdata
-out = out.split() #split by spaces
-out = str(out)
+if system == 'Windows':
+    try:
+        ping = subprocess.Popen(f"ping {target}",shell=True,  stdout=subprocess.PIPE)
+        out = ping.communicate()[0] #access stdoutdata
+        out = out.split() #split by spaces
+        out = str(out)
 
-ttl = re.search(r'TTL=(\d+)', out).group(1)
-ttl= int(ttl)
+        ttl = re.search(r'TTL=(\d+)', out).group(1)
+        ttl= int(ttl)
 
-if ttl >= 0 and ttl <= 64:
-    print("Operative System --> Linux")
-elif ttl >= 65 and ttl <= 128:
-    print("Operative System --> Windows")
+        if ttl >= 0 and ttl <= 64:
+            print("[!] Operative System --> Linux")
+        elif ttl >= 65 and ttl <= 128:
+            print("[!] Operative System --> Windows")
+        else:
+            print("Unkown")
+    except (AttributeError):
+        print('\n')
+        print('[!] An error occurred. It is possible that the server is down or that you have misspelled the IP or domain.')
+
+elif system == 'Linux':
+    ping = subprocess.Popen(f"ping -c 1 {target}",shell=True,  stdout=subprocess.PIPE)
+    out = ping.communicate()[0] #access stdoutdata
+    out = out.split() #split by spaces
+    out = str(out)
+
+    ttl = re.search(r'ttl=(\d+)', out).group(1)
+    ttl= int(ttl)
+
+    if ttl >= 0 and ttl <= 64:
+        print("[!] Operative System --> Linux")
+    elif ttl >= 65 and ttl <= 128:
+        print("[!] Operative System --> Windows")
+    else:
+        print("Unkown")
+
 else:
-    print("Unkown")
+    print('Bro Apple is for rich people. Are u fucking rich?')
